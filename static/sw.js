709c2529d6aaba02bfa7e3136b32c0afa32cdd1e
miramira111 <miraizu.f@gmail.com>
2026-05-04 16:44:27 +0900

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "estate-tools-v20260504-7";
+const CACHE_NAME = "estate-tools-v20260504-9";
 const STATIC_ASSETS = [
   "/static/logo.jpg",
   "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;600;700&display=swap",
